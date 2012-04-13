a1fddcadf90b57bce42ca3df24c1c59d0867991e
Jason Adam Young <jasonadamyoung@gmail.com>
2012-04-13 12:28:49 -0400

    bump version, add Changelog

---
+++ Changelog.md
@@ -0,0 +1,3 @@
+## 0.0.6 - Apr 13, 2012
+
+Fixes Gem.available? deprecation warnings - thanks to [Johan Kok](https://github.com/johankok)
--- lib/devserver/version.rb
+++ lib/devserver/version.rb
@@ -12,5 +12,5 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 module Devserver
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
